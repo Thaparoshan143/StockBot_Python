bb745f3bd6f3906569812736364491ffab5294ab
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-05-02 09:10:19 +0545

    fix: ssl certification, retry again if failed to resolve url

--- FinancialReportScrapper/Selenium.py
+++ FinancialReportScrapper/Selenium.py
@@ -5,17 +5,19 @@ import requests
 import os
 import time
 
+import ssl
+ssl._create_default_https_context = ssl._create_stdlib_context
 
 fin_URL = "https://merolagani.com/CompanyReports.aspx?type=QUARTERLY"
 search_year = "080/81"
 filter_year = "2080/81"
 filter_quarter = "third quarter".upper()
-loadmore_click_count = 0
+loadmore_click_count = 4
 should_open_window = False
 context_offset = "-".join(filter_quarter.split(" ")) + "-" + "-".join(filter_year.split("/"))
 out_folder_name = "FinReport" + "-" + context_offset
 out_folder_offset = os.getcwd() + "/" + out_folder_name
-per_delay_time = 2
+per_delay_time = 4
 should_overwrite_existing = True
 should_write_links = False
 
@@ -52,7 +54,7 @@ search_ele.click()
 
 loadmore_ele = active_driver.find_element(By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_divData"]/a')
 for i in range(0, loadmore_click_count):
-    time.sleep(per_delay_time/8)
+    time.sleep(per_delay_time/2)
     loadmore_ele.click()
 
 
@@ -85,12 +87,20 @@ else:
         script_symbol = active_driver.find_element(By.XPATH, '//*[@id="aspnetForm"]/div[4]/div[5]/div/div/table/tbody/tr[1]/td[2]/a').text.split(" ")[0]
         print(script_symbol)
         print(report_link)
-        active_driver.execute_script("window.scrollBy(0, 650)")
-        time.sleep(per_delay_time/2)
-        raw_img_ele = active_driver.find_element(By.XPATH, ' //*[@id="viewer"]/img')
-        img_src = raw_img_ele.get_attribute('src')
-        time.sleep(per_delay_time/2)
-        raw_img = requests.get(img_src).content
+        active_driver.execute_script("window.scrollBy(0, 700)")
+        while True:
+            try:
+                time.sleep(per_delay_time/4)
+                raw_img_ele = active_driver.find_element(By.XPATH, ' //*[@id="viewer"]/img')
+                img_src = raw_img_ele.get_attribute('src')
+                time.sleep(per_delay_time/4)
+                raw_img = requests.get(img_src).content
 
-        with open(out_folder_offset + "/" + script_symbol + "-" + report_link.split("id=")[-1]+".png", "wb") as nf:
-            nf.write(raw_img)
+                with open(out_folder_offset + "/" + script_symbol + "-" + report_link.split("id=")[-1]+".png", "wb") as nf:
+                    nf.write(raw_img)
+                print(" # Write succesful for : " + script_symbol)
+                break
+            except:
+                print("Couldn't resolve image url! trying again")
+                time.sleep(per_delay_time)
+                continue
